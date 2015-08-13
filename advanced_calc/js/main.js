940a2a3f404b4b7c678f6dc10aaa205754fdb433
Mike Jones <mikethespike77@gmail.com>
2015-08-13 18:49:51 +0100

    parsing value strings into numbers from input boxes

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -6,11 +6,12 @@ var button1 = document.getElementById('basic-calc');
 
 button1.addEventListener('click', function answer () {
 
-	var num1 = document.getElementById("basic-num-1").value;
+	var num1 = parseFloat(document.getElementById("basic-num-1").value);
 
-    var num2 = document.getElementById("basic-num-2").value;
+    var num2 = parseFloat(document.getElementById("basic-num-2").value);
 
     var op = document.getElementById("basic-operation");
+    
       switch(op){
       case "plus":
         answer = num1 + num2;
@@ -25,6 +26,8 @@ button1.addEventListener('click', function answer () {
         answer = num1 * num2;
         break;
     };
+
+
 }
 
 
