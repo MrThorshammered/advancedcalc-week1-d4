cee5d28ffab6b72d2fb417682e6f0104d0206e10
Mike Jones <mikethespike77@gmail.com>
2015-08-13 18:46:22 +0100

    info values being extracted

--- advanced_calc/js/main.js
+++ advanced_calc/js/main.js
@@ -1,1 +1,30 @@
 console.log("hello, mike");
+
+
+
+var button1 = document.getElementById('basic-calc');
+
+button1.addEventListener('click', function answer () {
+
+	var num1 = document.getElementById("basic-num-1").value;
+
+    var num2 = document.getElementById("basic-num-2").value;
+
+    var op = document.getElementById("basic-operation");
+      switch(op){
+      case "plus":
+        answer = num1 + num2;
+        break;
+      case "minus":
+        answer = num1 - num2;
+        break;
+      case "divided by":
+        answer = num1 / num2;
+        break;
+      case "multiplied by":
+        answer = num1 * num2;
+        break;
+    };
+}
+
+
